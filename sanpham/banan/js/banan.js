589be2645dc48183a331145419903f93d96d9187
KD <huynhthiainhi2636@gmail.com>
2025-06-30 07:38:20 +0700

    update sp

---
+++ sanpham/banan/js/banan.js
@@ -0,0 +1,148 @@
+const products = [
+  {
+    id: 1,
+    name: "Bàn ăn gỗ sồi Nga (6 ghế, 160x70-80)",
+    price: "15 triệu",
+    image: "banan_8.jpg",
+    rating: 4.8,
+    review: 25
+  },
+  {
+    id: 2,
+    name: "Bàn ăn gỗ sồi Nga (6 ghế)",
+    price: "9.5 triệu",
+    image: "banan_10.jpg",
+    rating: 4.6,
+    review: 18
+  },
+  {
+    id: 3,
+    name: "Bàn ăn mặt đá (6 ghế)",
+    price: "11 triệu",
+    image: "banan_11.jpg",
+    rating: 4.9,
+    review: 33
+  },
+  {
+    id: 4,
+    name: "Bàn ăn gỗ sồi (6 ghế, đệm)",
+    price: "16.5 triệu",
+    image: "banan_12.jpg",
+    rating: 4.7,
+    review: 22
+  },
+  {
+    id: 5,
+    name: "Bàn ăn hương đá (6 ghế, kính + đệm)",
+    price: "31 triệu",
+    image: "banan_13.jpg",
+    rating: 4.9,
+    review: 40
+  },
+  {
+    id: 6,
+    name: "Bàn ăn hương đá 100% (6 ghế, đệm)",
+    price: "39 triệu",
+    image: "banan_14.jpg",
+    rating: 5.0,
+    review: 52
+  },
+  {
+    id: 7,
+    name: "Bàn ăn hương xám (6 ghế, kính)",
+    price: "22.5 triệu",
+    image: "banan_15.jpg",
+    rating: 4.8,
+    review: 28
+  },
+  {
+    id: 8,
+    name: "Bàn ăn gỗ (6 ghế, đệm sáng)",
+    price: "21.5 triệu",
+    image: "banan_16.jpg",
+    rating: 4.7,
+    review: 20
+  },
+  {
+    id: 9,
+    name: "Bàn ăn gỗ (6 ghế, đệm nâu)",
+    price: "22.5 triệu",
+    image: "banan_17.jpg",
+    rating: 4.8,
+    review: 26
+  },
+  {
+    id: 10,
+    name: "Bàn ăn gỗ đỏ pachy 100% (6 ghế)",
+    price: "35 triệu",
+    image: "banan_18.jpg",
+    rating: 5.0,
+    review: 44
+  }
+  ,
+  {
+  id: 11,
+  name: "Bàn ăn 6 ghế  (mẫu chân u)",
+  price: "13,5 triệu",
+  image: "banan_1.jpg",
+  rating: 5.0,
+  review: 12
+},
+{
+  id: 12,
+  name: "Bàn ăn 6 ghế chân u lượn",
+  price: "13,5 triệu",
+  image: "banan_2.jpg",
+  rating: 5.0,
+  review: 7
+},
+{
+  id: 13,
+  name: "Bàn ăn 6 ghế gỗ sồi Nga đệm da đen",
+  price: "Liên hệ",
+  image: "banan_3.jpg",
+  rating: 4.5,
+  review: 5
+},
+{
+  id: 14,
+  name: "Bàn ăn tròn 8 ghế gỗ sồi đệm nâu",
+  price: "Liên hệ",
+  image: "banan_4.jpg",
+  rating: 4.0,
+  review: 10
+},
+{
+  id: 15,
+  name: "Bàn ăn 9 ghế không bao gồm đệm",
+  price: "Liên hệ",
+  image: "banan_5.jpg",
+  rating: 4.8,
+  review: 8
+}
+,
+{
+  id: 16,
+  name: "Bàn ăn 8 ghế gỗ sồi",
+  price: "13,5 triệu",
+  image: "banan_6.jpg",
+  rating: 4.8,
+  review: 8
+},
+{
+  id: 16,
+  name: "Bàn ăn 6 ghế gỗ sồi Nga",
+  price: "13,5 triệu",
+  image: "banan_7.jpg",
+  rating: 4.8,
+  review: 8
+},
+{
+  id: 16,
+  name: "Bàn ăn 6 ghế gỗ sồi Nga",
+  price: "15 triệu",
+  image: "banan_8.jpg",
+  rating: 4.8,
+  review: 8
+}
+];
---
+++ sanpham/sofa/chuyecamhung.py
@@ -0,0 +1,16 @@
+import os
+
+# Đường dẫn thư mục chứa video
+folder_path = r"F:\noithatkymoc\sanpham\sofa"  # ← Thay bằng đường dẫn thực tế
+
+# Lọc các file .mp4 và sắp xếp theo thứ tự
+mp4_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.jpg')])
+
+# Đổi tên từng file
+for idx, filename in enumerate(mp4_files, start=1):
+    old_path = os.path.join(folder_path, filename)
+    new_filename = f"sofa_{idx}.jpg"
+    new_path = os.path.join(folder_path, new_filename)
+
+    os.rename(old_path, new_path)
+    print(f"✅ Đã đổi: {filename} → {new_filename}")
